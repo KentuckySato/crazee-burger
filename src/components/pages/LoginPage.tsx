55aca56b7ffaa5f4b797bb673664fe0c86221316
Kentucky SATO <kentucky.sato@gmail.com>
2023-07-20 14:41:58 +0200

    ✨ F01: LOGIN > form ok

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,11 +1,27 @@
+import { useRef, useState } from "react"
+
 export default function LoginPage() {
+  // State
+  const [inputValue, setInputValue] = useState('');
+
+  // Effects
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue} !`);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  // Render
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
 
-      <input type="text" id="firstname" name="firstname" placeholder="Entrez votre prénom..." required />
+      <input onChange={handleChange} value={inputValue} type="text" id="firstname" name="firstname" placeholder="Entrez votre prénom..." required />
       <button type="submit">Accédez à votre espace</button>
     </form>
   )
