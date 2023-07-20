a079355c4c89f9ce450fb6e0dfe87a6a087c5481
Kentucky SATO <kentucky.sato@gmail.com>
2023-07-20 14:19:17 +0200

    ✨ F01: LOGIN > add input text and button submit

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -2,7 +2,11 @@ export default function LoginPage() {
   return (
     <form>
       <h1>Bienvenue chez nous !</h1>
+      <br />
       <h2>Connectez-vous</h2>
+
+      <input type="text" id="firstname" name="firstname" placeholder="Entrez votre prénom..." required />
+      <button type="submit">Accédez à votre espace</button>
     </form>
   )
 }
