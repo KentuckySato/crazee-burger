3c7ceeb6d1120e6e8f720a34cd7a4f5a7556fb1c
Kentucky SATO <kentucky.sato@gmail.com>
2023-07-22 16:13:01 +0200

    ✨ F02: ORDER > redirect to OrderPage with username

--- src/components/pages/login/LoginForm.tsx
+++ src/components/pages/login/LoginForm.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm() {
     // State
     const [inputValue, setInputValue] = useState('');
+    const navigate = useNavigate();
 
     // Effects
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (!inputValue) return alert('Veuillez entrer votre prénom !');
+
         setInputValue('');
+        navigate(`order/${inputValue}`);
     }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,7 +31,6 @@ export default function LoginForm() {
                 type="text" id="firstname" name="firstname" placeholder="Entrez votre prénom..." required
             />
             <button type="submit">Accédez à votre espace</button>
-            <Link to="/order">Vers OrderPage</Link>
         </form>
     )
 }
