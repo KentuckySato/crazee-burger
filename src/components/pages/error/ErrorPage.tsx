dc802f55b5e8b3912392241a4d51e6c070e75e75
Kentucky SATO <kentucky.sato@gmail.com>
2023-07-22 15:43:06 +0200

    ✨ F02: ERROR > add button go back home

--- src/components/pages/error/ErrorPage.tsx
+++ src/components/pages/error/ErrorPage.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+        <p>ErrorPage</p>
+        <Link to="/">
+            <button>Retourner vers la page d'accueil</button>
+        </Link>
+    </div>
   )
 }
