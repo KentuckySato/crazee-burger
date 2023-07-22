95a5f5b179da7c6b4ebe6cee7108cb16a7de0170
Kentucky SATO <kentucky.sato@gmail.com>
2023-07-22 15:40:33 +0200

    ✨ F02: APP > Install routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import LoginPage from './components/pages/login/LoginPage'
+import OrderPage from './components/pages/order/OrderPage'
+import ErrorPage from './components/pages/error/ErrorPage'
 
 function App() {
   return (
-    <LoginPage />
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   )
 }
 
