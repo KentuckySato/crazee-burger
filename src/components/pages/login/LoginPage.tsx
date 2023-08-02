904dfd2fa72e00509e0a4a4fba73c620e575ddea
Kentucky SATO <kentucky.sato@gmail.com>
2023-08-02 22:13:13 +0200

    💄 F03: LoginPage => improve style

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,13 +1,44 @@
 import LogoApp from "../../LogoApp"
 import LoginForm from "./LoginForm"
+import styled from 'styled-components';
 
 export default function LoginPage() {
 
   // Render
   return (
-    <div>
+    <LoginPageStyled>
       <LogoApp />
       <LoginForm />
-    </div>
+    </LoginPageStyled>
   )
 }
+
+const LoginPageStyled = styled.div`
+
+  height: 100vh;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+
+  &::before {
+    content: "";
+    background: url("/images/burger-background.jpg") center center / cover #000000b2;
+    background-blend-mode: darken;
+    position: absolute;
+    inset: 0px;
+    z-index: -1;
+  }
+
+  .logo {
+      transform: scale(2.5);
+  }
+
+  .loginForm {
+    width: 400px;
+    padding: 2.5rem 2rem;
+  }
+`;
