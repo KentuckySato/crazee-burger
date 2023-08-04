d2c9c00ae037329335467164ff7f9fd47cdf17de
Kentucky SATO <kentucky.sato@gmail.com>
2023-08-04 16:20:47 +0200

    ♻️ F04: OrderPage > provide username to navbar

--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -1,10 +1,12 @@
 import { Link, useParams } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
 
 export default function OrderPage() {
   const { username } = useParams();
 
   return (
     <div>
+      <Navbar username={username} />
       <h1>Bonjour { username }</h1>
       <Link to="/">
         <button>Déconnexion</button>
