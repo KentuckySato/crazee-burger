9592ecf3b8243dc4ae0d8bf7b2fe01cb4273edc2
Kentucky SATO <kentucky.sato@gmail.com>
2023-08-04 16:58:52 +0200

    💄 F04: OrderPage > styled wip

--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -1,16 +1,31 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
+import Main from "./Main/Main";
+import { styled } from "styled-components";
+import { theme } from "../../../theme";
 
 export default function OrderPage() {
   const { username } = useParams();
 
   return (
-    <div>
+    <OrderPageStyled className="container">
       <Navbar username={username} />
-      <h1>Bonjour { username }</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+      <Main />
+    </OrderPageStyled>
   )
 }
+
+const OrderPageStyled = styled.div`
+
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+    border-bottom-left-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    height: 95vh;
+    width: 1400px;
+    margin-top: auto;
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 0px 8px 0px;
+
+`;
