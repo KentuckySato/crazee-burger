86296033c5da3b0cff287a89358301648ed25662
Kentucky SATO <kentucky.sato@gmail.com>
2023-08-06 14:28:42 +0200

    ♻️ F06: NAVBAR > ToastAdmin > remove useless div

--- src/components/pages/order/Navbar/ToastAdmin.tsx
+++ src/components/pages/order/Navbar/ToastAdmin.tsx
@@ -1,32 +1,30 @@
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.min.css';
+import "react-toastify/dist/ReactToastify.min.css";
 import { styled } from "styled-components";
 import { theme } from "../../../../theme";
 
 export default function ToastAdmin() {
     return (
-        <ToastAdminStyled>
-            <ToastContainer className="toaster" bodyClassName="body-toast" />
-        </ToastAdminStyled>
-    )
+        <ToastContainerStyled className="toaster" bodyClassName="body-toast" />
+    );
 }
 
-const ToastAdminStyled = styled.div`
+const ToastContainerStyled = styled(ToastContainer)`
     .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
+        max-width: 300px;
     }
-    div {
-      line-height: 1.3em;
+
+    .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+        background: ${theme.colors.background_dark};
     }
-  }
+
+    .body-toast {
+        .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+            margin-right: 20px;
+            margin-left: 5px;
+        }
+        div {
+            line-height: 1.3em;
+        }
+    }
 `;
