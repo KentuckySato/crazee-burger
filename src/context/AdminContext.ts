74f4a9bb8eae79f470007276fe5922572441c9a8
Kentucky SATO <kentucky.sato@gmail.com>
2023-08-08 00:18:01 +0200

    ✨ F07: CONTEXT > create new AdminContext

---
+++ src/context/AdminContext.ts
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export type AdminContextType = {
+    isModeAdmin: boolean;
+    setIsModeAdmin: (setIsModeAdmin: boolean) => void;
+};
+
+export const AdminContext = createContext<AdminContextType>({
+    isModeAdmin: false,
+    setIsModeAdmin: () => {},
+});
