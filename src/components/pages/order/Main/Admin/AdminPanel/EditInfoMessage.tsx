686b02e7ecd022e0ed1b1090b11e0073b61adc0a
Kentucky SATO <kentucky.sato@gmail.com>
2023-08-29 19:13:08 +0200

    ✨ F09: ADMIN > new component EditInfoMessage

---
+++ src/components/pages/order/Main/Admin/AdminPanel/EditInfoMessage.tsx
@@ -0,0 +1,10 @@
+import { HiCursorClick } from 'react-icons/hi'
+
+export default function EditInfoMessage() {
+    return (
+        <div className="message">
+            <span>Cliquer sur un produit du menu pour le modifier</span>
+            <HiCursorClick />
+        </div>
+    )
+}
