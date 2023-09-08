30dff4202c7d7a19cf94a759a7159acd9a1fd001
Kentucky SATO <kentucky.sato@gmail.com>
2023-09-08 17:49:05 +0200

    F09: add new enum Product

---
+++ src/enums/product.ts
@@ -0,0 +1,13 @@
+export const EMPTY_PRODUCT = Object.freeze({
+    id: "",
+    title: "",
+    imageSource: "",
+    price: 0,
+});
+
+// export enum EMPTY_PRODUCT {
+//     price = 0,
+//     id = "",
+//     title = "",
+//     imageSource = "",
+// }
