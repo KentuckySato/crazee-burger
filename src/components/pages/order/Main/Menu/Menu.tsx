7d3b017dab1a5cae70236cee4b8ddea4c79dcabe
Kentucky SATO <kentucky.sato@gmail.com>
2023-09-19 23:58:52 +0200

    💄 F10: MAIN > MENU > Fix grid-template-columns to avoid auto placement of Card

--- src/components/pages/order/Main/Menu/Menu.tsx
+++ src/components/pages/order/Main/Menu/Menu.tsx
@@ -88,7 +88,7 @@ const MenuStyled = styled.div`
     background-color: ${theme.colors.background_white};
     flex: 1 1 0%;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-template-columns: repeat(3, 1fr);
     grid-template-rows: 1fr 1fr;
     grid-row-gap: 60px;
     padding: 50px 50px 150px;
