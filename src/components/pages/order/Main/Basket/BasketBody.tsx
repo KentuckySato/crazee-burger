e8a877f405a7b7257ff38e82faa24de604aded43
Kentucky SATO <kentucky.sato@gmail.com>
2023-09-23 22:25:47 +0200

    🚧 F11: EMPTY_BASKET > move css in EmptyBasket file

--- src/components/pages/order/Main/Basket/BasketBody.tsx
+++ src/components/pages/order/Main/Basket/BasketBody.tsx
@@ -24,19 +24,4 @@ const BasketBodyStyled = styled.div`
     flex: 1;
     background-color: ${theme.colors.background_white};
     box-shadow: ${theme.shadows.basket};
-
-    .empty-message {
-        display: flex;
-        height: calc(95vh - 10vh - 70px - 70px);
-        text-align: center;
-        flex: 1;
-        justify-content: center;
-        align-items: center;
-        align-items: center;
-        line-height: 2;
-        font-family: ${theme.fonts.family.stylish};
-        font-size: ${theme.fonts.size.P4};
-        color: ${theme.colors.greyBlue};
-
-    }
 `;
