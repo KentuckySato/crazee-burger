ea9afccf5c5b9c39937ce6233feefb1dd618c4c5
Kentucky SATO <kentucky.sato@gmail.com>
2023-10-04 23:47:56 +0200

    ♻️ F13: LOGIN > Add Welcome

--- src/components/pages/login/LoginForm.tsx
+++ src/components/pages/login/LoginForm.tsx
@@ -6,6 +6,7 @@ import InputText from '../../shared/InputText';
 import { BsChevronRight, BsPersonCircle } from "react-icons/bs"
 import Button from '../../shared/Button';
 import { authenticateUser } from '../../../api/user';
+import Welcome from './Welcome';
 
 export default function LoginForm() {
     // State
@@ -29,10 +30,7 @@ export default function LoginForm() {
 
     return (
         <LoginFormStyled onSubmit={handleSubmit} className='loginForm'>
-            <h1>Bienvenue chez nous !</h1>
-            <hr />
-            <h2>Connectez-vous</h2>
-
+            <Welcome />
             <InputText
                 leftIcon={<BsPersonCircle />}
                 name="firstname"
@@ -61,25 +59,6 @@ const LoginFormStyled = styled.form`
     margin: 0 auto;
     padding: 2.5rem ${theme.spacing.lg};
     border-radius: ${theme.borderRadius.round};
-
-    h1, h2 {
-        color: ${theme.colors.white};
-        font-weight: ${theme.fonts.weights.bold};
-    }
-
-    h1 {
-        font-size: ${theme.fonts.size.P5};
-    }
-
-    h2 {
-        font-size: ${theme.fonts.size.P4};
-        margin: 20px 10px 10px;
-    }
-
-    hr {
-        border: 1.5px solid ${theme.colors.loginLine};
-        margin-bottom: ${theme.gridUnit * 5}px;
-    }
 
     button {
         width: 100%;
