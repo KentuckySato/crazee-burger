5f7826dbdde13d7c2768b225c713cfeb9d1dcf95
Kentucky SATO <kentucky.sato@gmail.com>
2023-10-12 11:56:33 +0200

    ✨F14: BASKET > Total > Add CasinoEffect (finished)

--- src/components/shared/CasinoEffect.tsx
+++ src/components/shared/CasinoEffect.tsx
@@ -1,3 +1,5 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group"
+import styled from "styled-components"
 
 type CasinoEffectProps = {
     count: string
@@ -6,6 +8,37 @@ type CasinoEffectProps = {
 
 export default function CasinoEffect({ count, className }: CasinoEffectProps) {
     return (
-        <span className={className}>{count}</span>
+        <CasinoEffectStyled>
+            <TransitionGroup appear={true} component={null} className={"transition-group"}>
+                <CSSTransition key={count} classNames={"count-animated"} timeout={300}>
+                    <div className={className}>{count}</div>
+                </CSSTransition>
+            </TransitionGroup>
+        </CasinoEffectStyled>
     )
 }
+
+const CasinoEffectStyled = styled.div`
+    max-width: 100px;
+    position: relative;
+    overflow-y: hidden;
+
+    .count-animated-enter{
+        transform: translateY(100%);
+    }
+    .count-animated-enter-active{
+        transform: translateY(0%);
+        transition: 300ms;
+    }
+
+    .count-animated-exit{
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        transform: translateY(0%);
+    }
+    .count-animated-exit-active{
+        transition: 300ms;
+        transform: translateY(-100%);
+    }
+`;
