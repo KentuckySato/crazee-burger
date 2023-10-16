176783e15755e430f9f78507c435209f5a62b682
Kentucky SATO <kentucky.sato@gmail.com>
2023-10-16 18:45:32 +0200

    🚧  F15: BASKET > Refactor Basket

--- src/components/pages/order/Main/Basket/Basket.tsx
+++ src/components/pages/order/Main/Basket/Basket.tsx
@@ -1,20 +1,14 @@
 import styled from "styled-components";
 import Total from "./Total";
-import { OrderContext } from "../../../../../context/OrderContext";
-import { useContext } from "react";
 import { theme } from "../../../../../theme";
 import BasketFooter from "./BasketFooter";
 import BasketBody from "./BasketBody";
-import { isEmpty } from "../../../../../utils/array";
-import EmptyBasket from "./EmptyBasket";
 
 export default function Basket() {
-    const { basket, menu } = useContext(OrderContext)
-
     return (
         <BasketStyled>
             <Total />
-            {isEmpty(basket) ? <EmptyBasket isLoading={menu === undefined} /> : <BasketBody />}
+            <BasketBody />
             <BasketFooter />
         </BasketStyled>
     )
